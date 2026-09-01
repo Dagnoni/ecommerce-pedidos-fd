945806bac1d76bf5fd96ac887a58d79de755a3a5
Felipe Macari <felipefernandesmacari@gmail.com>
2026-09-01 16:17:13 -0300

    organizaçao de estrutura

--- back/src/main/java/ecommerce_pedidos/git/modelo/Cliente.java
+++ back/src/main/java/ecommerce_pedidos/git/modelo/Cliente.java
@@ -17,17 +17,17 @@ public class Cliente {
     
 
 
-public Cliente( String nome, String cpf, String email, String telefone, String rua, String bairro, Integer numeroCasa, String cidade, String estado, String paisNacao){
+    public Cliente( String nome, String cpf, String email, String telefone, String rua, String bairro, Integer numeroCasa, String cidade, String estado, String paisNacao){
     
-    this.nome = nome;
-    this.cpf = cpf;
-    this.email = email;
-    this.telefone = telefone;
-    this.rua = rua;
-    this.bairro = bairro;
-    this.numeroCasa = numeroCasa;
-    this.cidade = cidade;
-    this.estado = estado;
-    this.paisNacao = paisNacao;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.rua = rua;
+        this.bairro = bairro;
+        this.numeroCasa = numeroCasa;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.paisNacao = paisNacao;
     }
 }
