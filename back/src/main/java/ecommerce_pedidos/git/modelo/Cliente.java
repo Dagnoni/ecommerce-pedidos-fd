45ecef6c9e9772d284a53b00c97236e8e86ad27b
Felipe Macari <felipefernandesmacari@gmail.com>
2026-09-01 16:22:36 -0300

    finalizaçao do codigo

--- back/src/main/java/ecommerce_pedidos/git/modelo/Cliente.java
+++ back/src/main/java/ecommerce_pedidos/git/modelo/Cliente.java
@@ -30,4 +30,86 @@ public class Cliente {
         this.estado = estado;
         this.paisNacao = paisNacao;
     }
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public Integer getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(Integer numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPaisNacao() {
+        return paisNacao;
+    }
+
+    public void setPaisNacao(String paisNacao) {
+        this.paisNacao = paisNacao;
+    }
 }
